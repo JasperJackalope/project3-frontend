87664b9b7be0ff9267d0347afcfd1c5fb1fced2c
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-05-14 20:38:22 -0700

    Login Page

--- src/ThemeSwitcher.js
+++ src/ThemeSwitcher.js
@@ -1,45 +1,52 @@
 import { useState } from "react";
-import { Button, ButtonGroup, Dropdown } from "react-bootstrap";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 
-const ThemeSwitcher = () => {
+const Theme = () => {
   const [theme, setTheme] = useState(null);
-
+  const [dropdownOpen, setdropdownOpen] = useState(false);
   const resetTheme = () => {
     setTheme(null);
   };
 
   return (
     <div className="mb-2">
-      <Dropdown as={ButtonGroup} size="lg">
-        <Button
-          className="text-capitalize"
-          variant={theme ? theme : "secondary"}
+      <div
+        className={`text-capitalize h1 mb-4 w-100 text-center text-${
+          theme ? theme.toLowerCase() : "secondary"
+        }`}
+      >
+        {`${theme || "Default"}`}
+      </div>
+      <div className="d-flex justify-content-center p-5">
+        <Dropdown
+          isOpen={dropdownOpen}
+          toggle={() => setdropdownOpen(!dropdownOpen)}
         >
-          {theme ? theme : "Default"}
-        </Button>
-        <Dropdown.Toggle
-          split
-          variant={theme ? theme : "secondary"}
-          id="dropdown-split-basic"
-        />
-        <Dropdown.Menu>
-          <Dropdown.Item eventKey="1" onClick={() => setTheme("primary")}>
-            Primary
-          </Dropdown.Item>
-          <Dropdown.Item eventKey="2" onClick={() => setTheme("danger")}>
-            Danger
-          </Dropdown.Item>
-          <Dropdown.Item eventKey="3" onClick={() => setTheme("success")}>
-            Success
-          </Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item eventKey="4" onClick={resetTheme}>
-            Default Theme
-          </Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+          <DropdownToggle color={theme} caret>
+            Dropdown
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem onClick={() => setTheme("primary")}>
+              Primary Theme
+            </DropdownItem>
+            <DropdownItem onClick={() => setTheme("danger")}>
+              Danger Theme
+            </DropdownItem>
+            <DropdownItem onClick={() => setTheme("success")}>
+              Success Theme
+            </DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem onClick={resetTheme}>Default Theme</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
     </div>
   );
 };
 
-export default ThemeSwitcher;
+export default Theme;
