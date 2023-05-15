54f0c896974ada5c7512770616b3ca7cd1fc6b30
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-05-14 19:59:02 -0700

    Basic Booostrap React App

--- src/App.js
+++ src/App.js
@@ -1,49 +1,11 @@
-import React from 'react';
-import Header from './components/Header/Header';
-import AboutMe from './components/AboutMe/AboutMe';
-import Portfolio from './components/Portfolio/Portfolio';
-import Resume from './components/Resume/Resume';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
-import './assets/styles.css';
+import ThemeSwitcher from "./ThemeSwitcher";
 
 function App() {
-  const [route, setRoute] = React.useState(window.location.pathname);
-
-  React.useEffect(() => {
-    const handleRouteChange = () => {
-      setRoute(window.location.pathname);
-    };
-
-    window.addEventListener('popstate', handleRouteChange);
-
-    return () => {
-      window.removeEventListener('popstate', handleRouteChange);
-    };
-  }, []);
-
-  const renderRoute = () => {
-    switch (route) {
-      case '/':
-        return <AboutMe />;
-      case '/portfolio':
-        return <Portfolio />;
-      case '/resume':
-        return <Resume />;
-      case '/contact':
-        return <Contact />;
-      default:
-        return <AboutMe />;
-    }
-  };
-
   return (
-    <div className="App">
-      <Header />
-      <main>
-        {renderRoute()}
-      </main>
-      <Footer />
+    <div className="App min-vh-100 d-flex justify-content-center align-items-center">
+      <div>
+        <ThemeSwitcher />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
+import "./index.css";
+import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
 
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
